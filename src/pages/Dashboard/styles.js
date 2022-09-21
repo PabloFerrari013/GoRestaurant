70c2264cfebb122b6f69e72d1697c7953ee7ba6a
Pablo Ferrari <55794722+PabloFerrari013@users.noreply.github.com>
2022-09-21 17:01:18 -0300

    error login with github

---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const FoodsContainer = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 3vw;
+  margin-top: -140px;
+
+  display: flex;
+`
